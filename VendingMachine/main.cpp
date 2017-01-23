26f982945e3f0f8a441d71fda684ce4f67946998
Chandrasekar Sureshkumar <chandrusuresh@gmail.com>
2017-01-23 17:52:37 -0500

    Initial Commit

---
+++ VendingMachine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VendingMachine
+//
+//  Created by Chandrasekar Sureshkumar on 1/23/17.
+//  Copyright © 2017 Chandrasekar Sureshkumar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
